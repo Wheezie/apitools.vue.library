317e6bace046d26f817e8c22f26b5347df82c394
Wraptor <wraptor@wheezie.be>
2020-10-25 02:56:59 +0100

    Implement form components on login & registration

--- components/Login.vue
+++ components/Login.vue
@@ -1,21 +1,16 @@
 <template>
     <div id="login-holder">
         <form @submit.prevent="authenticate" >
-            <div class="input-field">
-                <label for="usernameInput">Username or email</label>
-                <input id="usernameInput" type="text" v-model="inputs.username" placeholder="Username/Email" />
-                <i class="material-icons">person_outline</i>
-            </div>
-            <div class="input-field">
-                <label for="passwordInput">Password</label>
-                <input id="passwordInput" type="password" v-model="inputs.password" placeholder="Password" />
-                <i class="material-icons">fingerprint</i>
-            </div>
-            <div class="input-field">
-                <input type="submit" value="Login" />
-            </div>
-            <router-link v-if="$props.showForgot" :to="{name: 'apiRegister'}">Register</router-link>
-            <router-link v-if="$props.showForgot" :to="{name: 'apiForgotPassword'}">Forgot password</router-link>
+            <Input-Field field="Username" name="Username/Email"
+                v-model="inputs.username" description="Username or email"
+                icon="person_outline" :validators="validators.required" />
+            <Input-Field field="Password" name="Password"
+                v-model="inputs.password" description="Password"
+                icon="fingerprint" type="password"
+                :validators="validators.required" />
+            <Submit-Button field="Login" />
+            <router-link v-if="showRegister ?? true" :to="{name: 'apiRegister'}">Register</router-link>
+            <router-link v-if="showForgot ?? true" :to="{name: 'apiForgotPassword'}">Forgot password</router-link>
         </form>
         <ul id="errors">
             <li v-for="error in errors" :key="error">{{ error }}</li>
@@ -24,7 +19,6 @@
 </template>
 
 <style scoped lang="scss">
-
     #errors {
         list-style:none;
     }
@@ -34,15 +28,23 @@
 import { defineComponent, onBeforeUnmount, onMounted } from 'vue';
 import { RouteLocationRaw } from 'vue-router';
 
-import { AuthenticationStatus } from '@/apitools/enums/AuthenticationStatus';
-import ApiService from '@/apitools/services/ApiService';
+import { AuthenticationStatus } from '../enums/AuthenticationStatus';
+import ApiService from '../services/ApiService';
 
-import { Subscription } from 'rxjs';
+import InputField from './forms/InputField.vue';
+import SubmitButton from './forms/SubmitButton.vue';
+import RequiredValidator from '../forms/validation/RequiredValidator';
+import StringValidator from '../forms/validation/StringValidator';
 
 export default defineComponent({
+    components: {
+        InputField,
+        SubmitButton
+    },
     props: {
         success: Object,
         showForgot: Boolean,
+        showRegister: Boolean
     },
 
     data(props) {
@@ -51,7 +53,10 @@ export default defineComponent({
                 username: '',
                 password: ''
             },
-            errors: Array<String>()
+            errors: Array<String>(),
+            validators: {
+                required: [ new RequiredValidator() ]
+            }
         }
     },
 
--- components/Register.vue
+++ components/Register.vue
@@ -1,40 +1,20 @@
 <template>
     <div id="register-holder">
         <form @submit.prevent="registrate">
-            <div class="input-field">
-                <label for="emailInput">Email</label>
-                <input id="emailInput" type="email" v-model="inputs.email" placeholder="Email" />
-                <i class="material-icons">alternate_email</i>
-            </div>
-            <div class="input-field">
-                <label for="usernameInput">Username</label>
-                <input id="usernameInput" type="text" v-model="inputs.username" placeholder="Username" />
-                <i class="material-icons">person_outline</i>
-            </div>
-            <div class="input-field">
-                <label for="firstNameInput">First name</label>
-                <input id="firstNameInput" type="text" v-model="inputs.firstName" placeholder="First name" />
-                <i class="material-icons">text_fields</i>
-            </div>
-            <div class="input-field">
-                <label for="lastNameInput">Last name</label>
-                <input id="lastNameInput" type="text" v-model="inputs.lastName" placeholder="Last name" />
-                <i class="material-icons">text_fields</i>
-            </div>
-            <div class="input-field">
-                <label for="passwordInput">Password</label>
-                <input id="passwordInput" type="password" v-model="inputs.password" placeholder="Password" />
-                <i class="material-icons">fingerprint</i>
-            </div>
-            <div class="input-field">
-                <label for="passwordRetypeInput">Retype Password</label>
-                <input id="passwordRetypeInput" type="password" v-model="inputs.passwordRetype" placeholder="Retype password" />
-                <i class="material-icons">fingerprint</i>
-            </div>
-            <div class="input-field">
-                <input type="submit" value="Register" />
-            </div>
-            <router-link v-if="$props.showLogin" :to="{name: 'apiLogin'}">Login</router-link>
+            <Input-Field field="Email"
+                type="email" icon="alternate_email" :validators="validators.email" />
+            <Input-Field field="Username"
+                type="text" icon="person_outline" :validators="validators.username" />
+            <Input-Field field="First Name"
+                type="text" icon="text_fields" :validators="validators.firstName" />
+            <Input-Field field="Last Name"
+                type="text" icon="text_fields" :validators="validators.lastName" />
+            <Input-Field field="Password"
+                type="password" icon="fingerprint" :validators="validators.password" />
+            <Input-Field field="Retype Password"
+                type="password" icon="fingerprint" :validators="validators.password" />
+            <Submit-Button field="Register" />
+            <router-link v-if="showLogin" :to="{name: 'apiLogin'}">Login</router-link>
         </form>
         <ul id="errors">
             <li v-for="error in errors" :key="error">{{ error }}</li>
@@ -52,16 +32,24 @@ import AccountService from '../services/AccountService';
 
 import RegistrationOptions from '../models/options/RegistrationOptions';
 
-import { Subscription } from 'rxjs';
+
+import InputField from './forms/InputField.vue';
+import SubmitButton from './forms/SubmitButton.vue';
+import StringValidator from '../forms/validation/StringValidator';
+import RequiredValidator from '../forms/validation/RequiredValidator';
 
 export default defineComponent({
+    components: {
+        InputField,
+        SubmitButton
+    },
     props: {
         success: Object,
         config: RegistrationOptions,
-        showLogin: Boolean,
+        showLogin: Boolean
     },
 
-    data(props) {
+    data() {
         return {
             inputs: {
                 email: '',
@@ -70,6 +58,14 @@ export default defineComponent({
                 lastName: '',
                 password: '',
                 passwordRetype: ''
+            },
+            validators: {
+                username: this.config!.userName.validators,
+                firstName: this.config!.firstName.validators,
+                lastName: this.config!.lastName.validators,
+                password: this.config!.password.validators,
+                email: this.config!.emailRequired
+                    ? [ new RequiredValidator() ] : []
             },
             errors: Array<String>()
         }
