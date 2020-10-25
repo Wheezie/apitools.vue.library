71a2383d44b9012867fc43970d4aad3c7b7f7a8b
Wraptor <wraptor@wheezie.be>
2020-10-25 02:56:15 +0100

    Submit button component

---
+++ components/forms/SubmitButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <input type="submit" :value="field" />
+</template>
+
+<style lang="scss" scoped>
+    input[type=submit] {
+        display: block;
+        padding: .8em;
+        margin:.6em 0;
+        border-radius: .25em;
+        z-index: 1;
+        outline: 0;
+
+        width: 100%;
+        border:0;
+        background-color: #7bc1ff;
+
+        &:hover {
+            cursor: pointer;
+            background-color: #2d89da;
+        }
+    }
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        field: String
+    }
+});
+</script>
