f04d7ae89e77f7e2aecff2b93f61898d84380d34
Wraptor <wraptor@wheezie.be>
2020-10-26 02:15:11 +0100

    IRegistrationObject reference fix

--- services/AccountService.ts
+++ services/AccountService.ts
@@ -1,11 +1,11 @@
 import { AxiosResponse } from 'axios';
 import { AuthenticationStatus } from '../enums/AuthenticationStatus';
 
-import IRegistrationRequest from '../models/account/RegistrationObject';
+import IRegistrationObject from '../models/account/IRegistrationObject';
 import ApiService from './ApiService';
 
 export default class AccountService {
-    static register(request: IRegistrationRequest, inviteToken?: String) {
+    static register(request: IRegistrationObject, inviteToken?: String) {
         ApiService.authentication.subscribe(c => {
             switch(c) {
                 case AuthenticationStatus.Unauthenticated:
